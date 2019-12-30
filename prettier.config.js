f28a1cc5b626a3cf4e192bcc9cb9a9cac51131da
Gurpreet Atwal <git@gatwal.com>
2019-12-30 13:43:59 -0800

    feat: create prettier config file

---
+++ prettier.config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @see https://prettier.io/docs/en/options.html
+ */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true, // default: false
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all', // default: none
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf', // default: auto
+};
